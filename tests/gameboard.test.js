8ecd9995f36ddb73a2d5f7ccae648892f342b896
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-24 15:54:23 +0800

    test: add destroyer to x orientation test

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -4,6 +4,7 @@ import { createShip } from "../src/logic/ships";
 const playerOne = gameboard();
 const playerTwo = gameboard();
 const carrier = createShip(5);
+const destroyer = createShip(3);
 
 test("Ship placing with x orientation", () => {
     playerOne.assignShip(carrier, 0, 0);
@@ -13,6 +14,12 @@ test("Ship placing with x orientation", () => {
     expect(playerOne.getBoard()[3][0][1]).toBe("ship");
     expect(playerOne.getBoard()[4][0][1]).toBe("ship");
     expect(playerOne.getBoard()[5][0][1]).toBe("water");
+    playerOne.assignShip(destroyer, 5, 6);
+    expect(playerOne.getBoard()[4][6][1]).toBe("water");
+    expect(playerOne.getBoard()[5][6][1]).toBe("ship");
+    expect(playerOne.getBoard()[6][6][1]).toBe("ship");
+    expect(playerOne.getBoard()[7][6][1]).toBe("ship");
+    expect(playerOne.getBoard()[8][6][1]).toBe("water");
 });
 
 test("Ship placing with x orientation", () => {

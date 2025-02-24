400f14bc60ab736bda3beb32277700b4be689520
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-24 15:50:56 +0800

    test: add ship placing test with y orientation

--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -4,12 +4,9 @@ import { createShip } from "../src/logic/ships";
 const playerOne = gameboard();
 const playerTwo = gameboard();
 const carrier = createShip(5);
-// playerTwo.assignShip(carrier, 0, 0, "y");
 
 test("Ship placing with x orientation", () => {
     playerOne.assignShip(carrier, 0, 0);
-    console.log(playerOne.getBoard());
-    console.log(carrier.getLength());
     expect(playerOne.getBoard()[0][0][1]).toBe("ship");
     expect(playerOne.getBoard()[1][0][1]).toBe("ship");
     expect(playerOne.getBoard()[2][0][1]).toBe("ship");
@@ -18,29 +15,12 @@ test("Ship placing with x orientation", () => {
     expect(playerOne.getBoard()[5][0][1]).toBe("water");
 });
 
-// test("Ship placing with y orientation", () => {
-//     expect(playerTwo.getXAxis()).toEqual([
-//         ["not hit", "ship"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//     ]);
-//     expect(playerTwo.getYAxis()).toEqual([
-//         ["not hit", "ship"],
-//         ["not hit", "ship"],
-//         ["not hit", "ship"],
-//         ["not hit", "ship"],
-//         ["not hit", "ship"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//         ["not hit", "water"],
-//     ]);
-// });
+test("Ship placing with x orientation", () => {
+    playerTwo.assignShip(carrier, 0, 0, "y");
+    expect(playerTwo.getBoard()[0][0][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][1][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][2][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][3][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][4][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][5][1]).toBe("water");
+});

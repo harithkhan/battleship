3cb89b57960c14002e61788bcf5fb69508c51c03
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-24 16:28:36 +0800

    feat: assign ship names when placed in coordinates

--- src/logic/gameboard.js
+++ src/logic/gameboard.js
@@ -1,3 +1,5 @@
+import { createShip } from "./ships";
+
 export const gameboard = () => {
     const board = [];
     for (let i = 0; i < 10; i++) {
@@ -11,18 +13,28 @@ export const gameboard = () => {
     const assignShip = (ship, xCoordinate, yCoordinate, orientation = "x") => {
         if (orientation === "x") {
             for (let i = 0; i < ship.getLength(); i++) {
-                board[xCoordinate + i][yCoordinate][1] = "ship";
+                board[xCoordinate + i][yCoordinate][1] = ship.getName();
             }
         }
         if (orientation === "y") {
             for (let i = 0; i < ship.getLength(); i++) {
-                board[xCoordinate][yCoordinate + i][1] = "ship";
+                board[xCoordinate][yCoordinate + i][1] = ship.getName();
             }
         }
     };
 
+    // const receiveAttack = (xCoordinate, yCoordinate) => {
+    //     if (board[xCoordinate][yCoordinate][0] === "not hit") {
+    //         board[xCoordinate][yCoordinate][0] = "hit";
+    //     }
+    //     if (board[xCoordinate][yCoordinate][1] === "ship") {
+
+    //     }
+    // };
+
     return {
         getBoard,
         assignShip,
+        // receiveAttack,
     };
 };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -8,32 +8,32 @@ const destroyer = createShip("test", 3);
 
 test("Ship placing with x orientation", () => {
     playerOne.assignShip(carrier, 0, 0);
-    expect(playerOne.getBoard()[0][0][1]).toBe("ship");
-    expect(playerOne.getBoard()[1][0][1]).toBe("ship");
-    expect(playerOne.getBoard()[2][0][1]).toBe("ship");
-    expect(playerOne.getBoard()[3][0][1]).toBe("ship");
-    expect(playerOne.getBoard()[4][0][1]).toBe("ship");
+    expect(playerOne.getBoard()[0][0][1]).toBe(carrier.getName());
+    expect(playerOne.getBoard()[1][0][1]).toBe(carrier.getName());
+    expect(playerOne.getBoard()[2][0][1]).toBe(carrier.getName());
+    expect(playerOne.getBoard()[3][0][1]).toBe(carrier.getName());
+    expect(playerOne.getBoard()[4][0][1]).toBe(carrier.getName());
     expect(playerOne.getBoard()[5][0][1]).toBe("water");
     playerOne.assignShip(destroyer, 5, 6);
     expect(playerOne.getBoard()[4][6][1]).toBe("water");
-    expect(playerOne.getBoard()[5][6][1]).toBe("ship");
-    expect(playerOne.getBoard()[6][6][1]).toBe("ship");
-    expect(playerOne.getBoard()[7][6][1]).toBe("ship");
+    expect(playerOne.getBoard()[5][6][1]).toBe(destroyer.getName());
+    expect(playerOne.getBoard()[6][6][1]).toBe(destroyer.getName());
+    expect(playerOne.getBoard()[7][6][1]).toBe(destroyer.getName());
     expect(playerOne.getBoard()[8][6][1]).toBe("water");
 });
 
 test("Ship placing with y orientation", () => {
     playerTwo.assignShip(carrier, 0, 0, "y");
-    expect(playerTwo.getBoard()[0][0][1]).toBe("ship");
-    expect(playerTwo.getBoard()[0][1][1]).toBe("ship");
-    expect(playerTwo.getBoard()[0][2][1]).toBe("ship");
-    expect(playerTwo.getBoard()[0][3][1]).toBe("ship");
-    expect(playerTwo.getBoard()[0][4][1]).toBe("ship");
+    expect(playerTwo.getBoard()[0][0][1]).toBe(carrier.getName());
+    expect(playerTwo.getBoard()[0][1][1]).toBe(carrier.getName());
+    expect(playerTwo.getBoard()[0][2][1]).toBe(carrier.getName());
+    expect(playerTwo.getBoard()[0][3][1]).toBe(carrier.getName());
+    expect(playerTwo.getBoard()[0][4][1]).toBe(carrier.getName());
     expect(playerTwo.getBoard()[0][5][1]).toBe("water");
     playerTwo.assignShip(destroyer, 5, 2, "y");
     expect(playerTwo.getBoard()[5][1][1]).toBe("water");
-    expect(playerTwo.getBoard()[5][2][1]).toBe("ship");
-    expect(playerTwo.getBoard()[5][3][1]).toBe("ship");
-    expect(playerTwo.getBoard()[5][4][1]).toBe("ship");
+    expect(playerTwo.getBoard()[5][2][1]).toBe(destroyer.getName());
+    expect(playerTwo.getBoard()[5][3][1]).toBe(destroyer.getName());
+    expect(playerTwo.getBoard()[5][4][1]).toBe(destroyer.getName());
     expect(playerTwo.getBoard()[5][5][1]).toBe("water");
 });

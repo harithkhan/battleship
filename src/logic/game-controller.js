8e6d443f19c7d78f00d0de85d7fd4783dfdd4481
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-25 16:36:22 +0800

    feat: add resetGameState function

--- src/logic/game-controller.js
+++ src/logic/game-controller.js
@@ -1,10 +1,3 @@
-import { gameboard } from "./gameboard";
-import { createShip } from "./ships";
-import { players } from "./players";
-
-const humanPlayerOne = players("human", "Player 1");
-const computerPlayer = players("computer", "Computer");
-
 const gameState = {
     gameStart: false,
     gameOver: false,
@@ -20,6 +13,7 @@ export const getGameState = () => gameState;
 
 export const gameStart = () => {
     gameState.gameStart = true;
+    gameState.playerTurn = gameState.players.playerOne;
 };
 
 export const gameOver = () => {
@@ -27,10 +21,19 @@ export const gameOver = () => {
     gameState.gameStart = false;
 };
 
-export const assignPlayerOne = (player) => {
-    gameState.players.playerOne = player;
-};
+// export const assignPlayerOne = (player) => {
+//     gameState.players.playerOne = player;
+// };
 
-export const assignPlayerTwo = (player) => {
-    gameState.players.playerTwo = player;
+// export const assignPlayerTwo = (player) => {
+//     gameState.players.playerTwo = player;
+// };
+
+export const resetGameState = () => {
+    gameState.gameStart = false;
+    gameState.gameOver = false;
+    gameState.players.playerOne = null;
+    gameState.players.playerTwo = null;
+    gameState.playerTurn = null;
+    gameState.gameWinner = null;
 };

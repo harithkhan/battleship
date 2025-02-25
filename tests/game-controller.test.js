af837b50136e3dee55074422eedec341e4df96a1
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-25 16:36:01 +0800

    test: add test for resetGameState()

--- tests/game-controller.test.js
+++ tests/game-controller.test.js
@@ -3,8 +3,9 @@ import {
     getGameState,
     gameStart,
     gameOver,
-    assignPlayerOne,
-    assignPlayerTwo,
+    resetGameState,
+    // assignPlayerOne,
+    // assignPlayerTwo,
 } from "../src/logic/game-controller";
 
 test("Get game state", () => {
@@ -32,11 +33,35 @@ test("Game can end", () => {
     expect(getGameState().gameOver).toBeTruthy();
 });
 
-test("Can assign players to game", () => {
-    const humanPlayerOne = players("human", "Player 1");
-    const computerPlayer = players("computer", "Computer");
-    assignPlayerOne(humanPlayerOne);
-    assignPlayerTwo(computerPlayer);
-    expect(getGameState().players.playerOne.getName()).toBe("Player 1");
-    expect(getGameState().players.playerTwo.getName()).toBe("Computer");
+test("Game can reset", () => {
+    gameStart();
+    resetGameState();
+    expect(getGameState()).toEqual({
+        gameStart: false,
+        gameOver: false,
+        players: {
+            playerOne: null,
+            playerTwo: null,
+        },
+        playerTurn: null,
+        gameWinner: null,
+    });
 });
+
+// describe("Player assignments", () => {
+//     const humanPlayerOne = players("human", "Player 1");
+//     const computerPlayer = players("computer", "Computer");
+//     assignPlayerOne(humanPlayerOne);
+//     assignPlayerTwo(computerPlayer);
+//     test("Can assign players to game", () => {
+//         expect(getGameState().players.playerOne.getName()).toBe("Player 1");
+//         expect(getGameState().players.playerTwo.getName()).toBe("Computer");
+//     });
+//     test("Before game starts, player turn is null", () => {
+//         expect(getGameState().playerTurn).toBe(null);
+//     });
+//     test("Game start assigns first player correctly", () => {
+//         gameStart();
+//         expect(getGameState().playerTurn.getName()).toBe("Player 1");
+//     });
+// });

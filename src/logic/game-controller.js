10fa61deb08f323be738aa44ae0cdfe4346a6738
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-25 16:07:14 +0800

    feat: add gameOver function to game-controller.js

--- src/logic/game-controller.js
+++ src/logic/game-controller.js
@@ -21,3 +21,8 @@ export const getGameState = () => gameState;
 export const gameStart = () => {
     gameState.gameStart = true;
 };
+
+export const gameOver = () => {
+    gameState.gameOver = true;
+    gameState.gameStart = false;
+};

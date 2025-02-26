fa9c4fad5394027bd5c9ac0c8ab0b87be3096c7c
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-26 14:12:11 +0800

    feat: add coordinates to board grid datasets

--- src/ui/player-boards.js
+++ src/ui/player-boards.js
@@ -2,17 +2,29 @@ const playerOneBoard = document.querySelector(".player-one-board-container");
 const playerTwoBoard = document.querySelector(".player-two-board-container");
 
 export function createPlayerOneBoard() {
+    let initialCoordinates = [0, 9];
     for (let i = 0; i < 100; i++) {
         const gridBox = document.createElement("div");
         gridBox.className = "grid-box";
+        gridBox.dataset.coordinates = initialCoordinates.join(",");
         playerOneBoard.appendChild(gridBox);
+        initialCoordinates = [initialCoordinates[0] + 1, initialCoordinates[1]];
+        if (initialCoordinates[0] > 9) {
+            initialCoordinates = [0, initialCoordinates[1] - 1];
+        }
     }
 }
 
 export function createPlayerTwoBoard() {
+    let initialCoordinates = [0, 9];
     for (let i = 0; i < 100; i++) {
         const gridBox = document.createElement("div");
         gridBox.className = "grid-box";
+        gridBox.dataset.coordinates = initialCoordinates.join(",");
         playerTwoBoard.appendChild(gridBox);
+        initialCoordinates = [initialCoordinates[0] + 1, initialCoordinates[1]];
+        if (initialCoordinates[0] > 9) {
+            initialCoordinates = [0, initialCoordinates[1] - 1];
+        }
     }
 }

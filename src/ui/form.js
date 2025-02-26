eae99c7d5a997e9a98dcefe439ca234f195a038a
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-26 20:07:12 +0800

    feat: add logic to handle game form submission Add logic to set game mode upon form submission. Add logic to assign players and start game upon form submission.

--- src/ui/form.js
+++ src/ui/form.js
@@ -1,8 +1,8 @@
-const gameForm = document.querySelector(".game-form");
-const singlePlayerButton = document.getElementById("single-player");
+import * as gameController from "../logic/game-controller";
+import { players } from "../logic/players";
+
 const twoPlayerButton = document.getElementById("two-player");
 const playerOneInput = document.getElementById("input-player-1");
-
 function handleTwoPlayerClick() {
     const previousInput = document.getElementById("input-player-2");
     if (!previousInput) {
@@ -15,6 +15,7 @@ function handleTwoPlayerClick() {
     }
 }
 
+const singlePlayerButton = document.getElementById("single-player");
 function handleSinglePlayerClick() {
     const playerTwoInput = document.getElementById("input-player-2");
     if (playerTwoInput) {
@@ -22,7 +23,37 @@ function handleSinglePlayerClick() {
     }
 }
 
+const gameForm = document.querySelector(".game-form");
+const dialog = document.querySelector(".start-game-dialog");
+const startButton = document.getElementById("start-button");
+function handleStartClick(event) {
+    event.preventDefault();
+    const formData = new FormData(gameForm);
+    const gameMode = formData.get("game-mode");
+    gameController.setGameMode(gameMode);
+    if (gameController.getGameMode() === "single-player") {
+        const playerOneName = formData.get("input-player-1") || "Player 1";
+        const playerOne = players("human", playerOneName);
+        const playerTwo = players("computer", "Computer");
+        gameController.assignPlayerOne(playerOne);
+        gameController.assignPlayerTwo(playerTwo);
+        gameController.gameStart();
+        dialog.close();
+    }
+    if (gameController.getGameMode() === "two-player") {
+        const playerOneName = formData.get("input-player-1") || "Player 1";
+        const playerOne = players("human", playerOneName);
+        const playerTwoName = formData.get("input-player-2") || "Player 2";
+        const playerTwo = players("human", playerTwoName);
+        gameController.assignPlayerOne(playerOne);
+        gameController.assignPlayerTwo(playerTwo);
+        gameController.gameStart();
+        dialog.close();
+    }
+}
+
 export function attachFormEventListeners() {
     twoPlayerButton.addEventListener("click", handleTwoPlayerClick);
     singlePlayerButton.addEventListener("click", handleSinglePlayerClick);
+    startButton.addEventListener("click", handleStartClick);
 }

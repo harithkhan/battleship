8af8c02a2c1e9026479183308e1725e8e6524c77
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-27 09:52:23 +0800

    fix: reduce playerTurn display sentence

--- src/ui/information-display.js
+++ src/ui/information-display.js
@@ -3,5 +3,5 @@ import * as gameController from "../logic/game-controller";
 export function displayPlayerTurn() {
     const playerTurn = gameController.getPlayerTurn().getName();
     const display = document.querySelector(".game-information-display");
-    display.textContent = `It is ${playerTurn}'s turn`;
+    display.textContent = `${playerTurn}'s turn`;
 }

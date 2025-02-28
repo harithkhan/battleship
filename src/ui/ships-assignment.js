ad3f343074cc9d7f4072cc6c66206de664ea08f1
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-28 12:52:36 +0800

    feat: add random assignment of computer ships

--- src/ui/ships-assignment.js
+++ src/ui/ships-assignment.js
@@ -1,5 +1,3 @@
-// Module populates boards with mock positions for testing
-// Not meant to be part of the final game, only exists for testing
 import { players } from "../logic/players";
 import { playerOneShips, playerTwoShips } from "../logic/ships";
 import * as gameController from "../logic/game-controller";
@@ -40,21 +38,37 @@ export function assignShipsTest() {
     playerTwo.getBoard().assignShip(playerTwoShips.patrolBoat, 5, 8, "x");
 }
 
-function randomCoordinates() {
+function randomComputerCoordinates(ship) {
     let xCoordinate = Math.floor(Math.random() * 10);
     let yCoordinate = Math.floor(Math.random() * 10);
     const computerBoard = gameController.getPlayerTwo().getBoard().getBoard();
+    const xList = [];
+    const yList = [];
+    // computerBoard.forEach((x) => {
+    //     computerBoard.forEach((y) => {
+    //         if (y[1] !== "water") {
+    //             xList.push(computerBoard.indexOf(x));
+    //             yList.push(x.indexOf(y));
+    //         }
+    //     });
+    // });
+    computerBoard.forEach((x) => {
+        xList.push(x);
+    });
     computerBoard.forEach((x) => {
         x.forEach((y) => {
-            if (
-                computerBoard.indexOf(x) === xCoordinate ||
-                x.indexOf(y) === yCoordinate
-            ) {
-                xCoordinate = Math.floor(Math.random() * 10);
-                yCoordinate = Math.floor(Math.random() * 10);
-            }
+            yList.push(y);
         });
     });
+    while (
+        xList.includes(xCoordinate) ||
+        yList.includes(yCoordinate) ||
+        ship.getLength() + xCoordinate > 9 ||
+        ship.getLength() + yCoordinate > 9
+    ) {
+        xCoordinate = Math.floor(Math.random() * 10);
+        yCoordinate = Math.floor(Math.random() * 10);
+    }
     return [xCoordinate, yCoordinate];
 }
 
@@ -68,9 +82,49 @@ function randomOrientation() {
 
 export function assignComputerShips() {
     const playerTwo = gameController.getPlayerTwo();
-    playerTwo.getBoard().assignShip(playerTwoShips.carrier, 0, 0, "x");
-    playerTwo.getBoard().assignShip(playerTwoShips.battleship, 9, 3, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.destroyer, 6, 3, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.submarine, 1, 5, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.patrolBoat, 5, 8, "x");
+    const carrierCoordinates = randomComputerCoordinates(playerTwoShips.carrier);
+    const battleshipCoordinates = randomComputerCoordinates(playerTwoShips.battleship);
+    const destroyerCoordinates = randomComputerCoordinates(playerTwoShips.destroyer);
+    const submarineCoordinates = randomComputerCoordinates(playerTwoShips.submarine);
+    const patrolBoatCoordinates = randomComputerCoordinates(playerTwoShips.patrolBoat);
+    playerTwo
+        .getBoard()
+        .assignShip(
+            playerTwoShips.carrier,
+            carrierCoordinates[0],
+            carrierCoordinates[1],
+            randomOrientation()
+        );
+    playerTwo
+        .getBoard()
+        .assignShip(
+            playerTwoShips.battleship,
+            battleshipCoordinates[0],
+            battleshipCoordinates[1],
+            randomOrientation()
+        );
+    playerTwo
+        .getBoard()
+        .assignShip(
+            playerTwoShips.destroyer,
+            destroyerCoordinates[0],
+            destroyerCoordinates[1],
+            randomOrientation()
+        );
+    playerTwo
+        .getBoard()
+        .assignShip(
+            playerTwoShips.submarine,
+            submarineCoordinates[0],
+            submarineCoordinates[1],
+            randomOrientation()
+        );
+    playerTwo
+        .getBoard()
+        .assignShip(
+            playerTwoShips.patrolBoat,
+            patrolBoatCoordinates[0],
+            patrolBoatCoordinates[1],
+            randomOrientation()
+        );
 }

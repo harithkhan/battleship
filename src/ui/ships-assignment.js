6f25de9b338827ffe1851a97cb09b2e93f08d992
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-02-28 23:49:51 +0800

    fix: remove mock assign ships code

--- src/ui/ships-assignment.js
+++ src/ui/ships-assignment.js
@@ -1,42 +1,5 @@
-import { players } from "../logic/players";
 import { playerOneShips, playerTwoShips } from "../logic/ships";
 import * as gameController from "../logic/game-controller";
-
-export function executePopulationTest() {
-    gameController.resetGameState();
-    const playerOne = players("human", "Player 1");
-    const computerPlayer = players("computer", "Computer");
-    gameController.assignPlayerOne(playerOne);
-    gameController.assignPlayerTwo(computerPlayer);
-
-    playerOne.getBoard().assignShip(playerOneShips.carrier, 0, 0, "x");
-    playerOne.getBoard().assignShip(playerOneShips.battleship, 9, 0, "y");
-    playerOne.getBoard().assignShip(playerOneShips.destroyer, 3, 2, "x");
-    playerOne.getBoard().assignShip(playerOneShips.submarine, 1, 5, "y");
-    playerOne.getBoard().assignShip(playerOneShips.patrolBoat, 3, 8, "x");
-
-    computerPlayer.getBoard().assignShip(playerTwoShips.carrier, 0, 0, "x");
-    computerPlayer.getBoard().assignShip(playerTwoShips.battleship, 9, 3, "y");
-    computerPlayer.getBoard().assignShip(playerTwoShips.destroyer, 6, 3, "y");
-    computerPlayer.getBoard().assignShip(playerTwoShips.submarine, 1, 5, "y");
-    computerPlayer.getBoard().assignShip(playerTwoShips.patrolBoat, 5, 8, "x");
-}
-
-export function assignShipsTest() {
-    const playerOne = gameController.getPlayerOne();
-    const playerTwo = gameController.getPlayerTwo();
-    playerOne.getBoard().assignShip(playerOneShips.carrier, 0, 0, "x");
-    playerOne.getBoard().assignShip(playerOneShips.battleship, 9, 0, "y");
-    playerOne.getBoard().assignShip(playerOneShips.destroyer, 3, 2, "x");
-    playerOne.getBoard().assignShip(playerOneShips.submarine, 1, 5, "y");
-    playerOne.getBoard().assignShip(playerOneShips.patrolBoat, 3, 8, "x");
-
-    playerTwo.getBoard().assignShip(playerTwoShips.carrier, 0, 0, "x");
-    playerTwo.getBoard().assignShip(playerTwoShips.battleship, 9, 3, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.destroyer, 6, 3, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.submarine, 1, 5, "y");
-    playerTwo.getBoard().assignShip(playerTwoShips.patrolBoat, 5, 8, "x");
-}
 
 function checkAdjacentCoordinates(orientation, ship, player, x, y) {
     if (orientation === "x") {
@@ -108,11 +71,11 @@ export function assignComputerShips() {
     const destroyerOrientation = randomOrientation();
     const submarineOrientation = randomOrientation();
     const patrolBoatOrientation = randomOrientation();
+
     const carrierCoordinates = randomComputerCoordinates(
         playerTwoShips.carrier,
         carrierOrientation
     );
-
     playerTwo
         .getBoard()
         .assignShip(

74d42d066c0b5f5c2b8603847c98b9cb58f4ba7d
Harith Khan <184179594+harithkhan@users.noreply.github.com>
2025-03-02 21:56:30 +0800

    refactor: merge attack and pass-device modules

--- src/ui/attack.js
+++ src/ui/attack.js
@@ -10,7 +10,8 @@ import {
     setGameWinner,
     getOtherPlayer,
 } from "../logic/game-controller";
-import { insertPassDeviceButton } from "./pass-device";
+import { hideShips } from "./hide-ships";
+import { renderPlayerOneBoard, renderPlayerTwoBoard } from "./render.boards";
 
 function attack(player, coordinates) {
     const attackedBox =
@@ -162,4 +163,150 @@ export function playComputerAttack() {
         draftCoordinates = generateRandomCoordinates().join(",");
     }
     attack("player-one", draftCoordinates);
+}
+
+const gameButtonsContainer = document.querySelector(".game-buttons-container");
+
+function removePlayerOneAttackListeners() {
+    const playerOneGridBoxes = document.querySelectorAll(
+        ".player-one-board-container > .grid-box"
+    );
+    const playerOneBoard = getPlayerOne().getBoard().getBoard();
+    playerOneBoard.forEach((xCoordinate) => {
+        xCoordinate.forEach((yCoordinate) => {
+            if (yCoordinate[0] === "not hit") {
+                const coordinatesStr = [
+                    playerOneBoard.indexOf(xCoordinate),
+                    xCoordinate.indexOf(yCoordinate),
+                ].join(",");
+                playerOneGridBoxes.forEach((gridBox) => {
+                    if (gridBox.dataset.coordinates === coordinatesStr) {
+                        gridBox.removeEventListener("click", handleAttack);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function addPlayerTwoAttackListeners() {
+    const playerTwoGridBoxes = document.querySelectorAll(
+        ".player-two-board-container > .grid-box"
+    );
+    const playerTwoBoard = getPlayerTwo().getBoard().getBoard();
+    playerTwoBoard.forEach((xCoordinate) => {
+        xCoordinate.forEach((yCoordinate) => {
+            if (yCoordinate[0] === "not hit") {
+                const coordinatesStr = [
+                    playerTwoBoard.indexOf(xCoordinate),
+                    xCoordinate.indexOf(yCoordinate),
+                ].join(",");
+                playerTwoGridBoxes.forEach((gridBox) => {
+                    if (gridBox.dataset.coordinates === coordinatesStr) {
+                        gridBox.addEventListener("click", handleAttack);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function removePlayerTwoAttackListeners() {
+    const playerTwoGridBoxes = document.querySelectorAll(
+        ".player-two-board-container > .grid-box"
+    );
+    const playerTwoBoard = getPlayerTwo().getBoard().getBoard();
+    playerTwoBoard.forEach((xCoordinate) => {
+        xCoordinate.forEach((yCoordinate) => {
+            if (yCoordinate[0] === "not hit") {
+                const coordinatesStr = [
+                    playerTwoBoard.indexOf(xCoordinate),
+                    xCoordinate.indexOf(yCoordinate),
+                ].join(",");
+                playerTwoGridBoxes.forEach((gridBox) => {
+                    if (gridBox.dataset.coordinates === coordinatesStr) {
+                        gridBox.removeEventListener("click", handleAttack);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function addPlayerOneAttackListeners() {
+    const playerOneGridBoxes = document.querySelectorAll(
+        ".player-one-board-container > .grid-box"
+    );
+    const playerOneBoard = getPlayerOne().getBoard().getBoard();
+    playerOneBoard.forEach((xCoordinate) => {
+        xCoordinate.forEach((yCoordinate) => {
+            if (yCoordinate[0] === "not hit") {
+                const coordinatesStr = [
+                    playerOneBoard.indexOf(xCoordinate),
+                    xCoordinate.indexOf(yCoordinate),
+                ].join(",");
+                playerOneGridBoxes.forEach((gridBox) => {
+                    if (gridBox.dataset.coordinates === coordinatesStr) {
+                        gridBox.addEventListener("click", handleAttack);
+                    }
+                });
+            }
+        });
+    });
+}
+
+function receiveDevice() {
+    if (getPlayerTurn() === getPlayerOne()) {
+        removePlayerOneAttackListeners();
+        addPlayerTwoAttackListeners();
+        renderPlayerOneBoard();
+    }
+    if (getPlayerTurn() === getPlayerTwo()) {
+        removePlayerTwoAttackListeners();
+        addPlayerOneAttackListeners();
+        renderPlayerTwoBoard();
+    }
+    const receiveDeviceButton = document.querySelector(
+        ".receive-device-button"
+    );
+    receiveDeviceButton.remove();
+}
+
+export function insertReceiveDeviceButton() {
+    const receiveDeviceButton = document.createElement("button");
+    receiveDeviceButton.className = "receive-device-button";
+    receiveDeviceButton.type = "button";
+    receiveDeviceButton.textContent = "Receive Device";
+    receiveDeviceButton.addEventListener("click", receiveDevice);
+    gameButtonsContainer.appendChild(receiveDeviceButton);
+}
+
+function passDevice() {
+    hideShips(getPlayerOne());
+    hideShips(getPlayerTwo());
+    const passDeviceButton = document.querySelector(".pass-device-button");
+    passDeviceButton.remove();
+    insertReceiveDeviceButton();
+
+    const playerOneGridBoxes = document.querySelectorAll(
+        ".player-one-board-container > .grid-box"
+    );
+    const playerTwoGridBoxes = document.querySelectorAll(
+        ".player-two-board-container > .grid-box"
+    );
+    playerOneGridBoxes.forEach((gridBox) => {
+        gridBox.removeEventListener("click", handleAttack);
+    });
+    playerTwoGridBoxes.forEach((gridBox) => {
+        gridBox.removeEventListener("click", handleAttack);
+    });
+}
+
+export function insertPassDeviceButton() {
+    const passDeviceButton = document.createElement("button");
+    passDeviceButton.className = "pass-device-button";
+    passDeviceButton.type = "button";
+    passDeviceButton.textContent = "Pass Device";
+    passDeviceButton.addEventListener("click", passDevice);
+    gameButtonsContainer.appendChild(passDeviceButton);
 }
